9eb2a98c1d3bd9d43c58b3bb5022e2b7a35c4a94
Shayan Ullah Siddiqui <71758954+ShayanUllahSiddiqui@users.noreply.github.com>
2020-12-18 16:55:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const card = document.getElementById('card');
+
+card.addEventListener('click', () => {
+  let text = [];
+  text.innerHtml = window.alert("Blog not availble yet, stay tuned!");  
+});
